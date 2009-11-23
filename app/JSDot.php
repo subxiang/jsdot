2be2045fb60f29e3063e3b290ecc78fb84d6fc59
nicos.giuliani <nicos.giuliani@e0888478-d5d9-11de-b7d7-53e004a27ad6>
2009-11-23 15:44:14 +0000

    First svg element

--- app/JSDot.php
+++ app/JSDot.php
@@ -1,9 +1,11 @@
-
 <?php 
-	$libs = array("jquery-1.3.2.js", "jquery-ui-1.7.2.js");
-	foreach($libs as $lib) {
-		require_once "lib/".$lib;
-	}
 	
 	header("Content-type: text/javascript");
+
+	$libs = array("lib/jquery-1.3.2.js", "lib/jquery-ui-1.7.2.js","core/main.js");
+	
+	foreach($libs as $lib) {
+		require_once $lib;
+	}
+	
 ?>
---
+++ app/core/main.js
@@ -0,0 +1,25 @@
+function Class(){
+    return function(arguments){
+        this.init(arguments);
+    }
+};
+
+var JSDot = new Class();
+
+JSDot.prototype = {
+
+
+    mainWin: null,
+    
+    init: function(){
+
+        this.mainWin = document.createElement("embed");
+		this.mainWin.style.cssText = "position:absolute;top:0;left:0;right:0;bottom:0;";
+		this.mainWin.src = "app/frame/win.svg";
+        document.body.appendChild(this.mainWin);
+        
+    }
+    
+};
+
+
