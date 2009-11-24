f55a1d6892665f9ce93db5b56e4b784a9fce0384
luciablondel <luciablondel@gmail.com>
2009-11-24 15:55:26 +0000

    update condition for not drawing in the menu part

--- app/core/main.js
+++ app/core/main.js
@@ -8,18 +8,21 @@ var JSDot = new Class();
 
 JSDot.prototype = {
 
-
     mainWin: null,
+	selected_form: null,
     
     init: function(){
-
         this.mainWin = document.createElement("embed");
 		this.mainWin.style.cssText = "position:absolute;top:0;left:0;right:0;bottom:0;";
 		this.mainWin.src = "app/frame/win.svg";
         document.body.appendChild(this.mainWin);
-        
-    }
+    },
     
+	 add_figure: function(form) {
+		selected_form = form;
+	}
 };
+
+
 
 
