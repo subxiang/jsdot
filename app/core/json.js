092077cda284ff2c9bab0af4782caba0fe8a9235
silhusk <silhusk@e0888478-d5d9-11de-b7d7-53e004a27ad6>
2009-11-26 15:57:52 +0000

    added conversion from JSON representation to internal graph representation

--- app/core/json.js
+++ app/core/json.js
@@ -1,4 +1,50 @@
 
-function loadJSON() {
+/** Returns a new instance of a GraphItem.
+ * GraphItems represent graphs, edges and nodes.
+ * Usage: var g = newGraph();
+ */
+function newGraphItem() {
+	var res = {};
+	res.name = "";
+	res.attributes = {};
 	
+	return res;
+}
+
+Graph.prototype.setLabel = function (label) {
+	this.label = label;
+}
+
+/** Load a graph from the JSON representation.
+ * 
+ */
+function loadJSON(jg) {
+	var nodes_ref = {};
+	var g = newGraphItem();
+	
+	g.name = jg.name;
+	g.directed = jg.directed;
+	g.attributes = new Object(jg.attributes);
+	g.nodes = [];
+	g.edges = [];
+	
+	for (var i=0; i < jg.nodes.length; i++) {
+		var jn = jg.nodes[i];
+		var n = newGraphItem();
+		n.name = jn.name;
+		n.attributes = new Object(jn.attributes);
+		g.nodes[i] = n;
+		// FIXME: check if n.name is already defined
+		nodes_ref[n.name] = n;
+	}
+	
+	for (var i=0; i < jg.edges.length; i++) {
+		var je = jg.edges[i];
+		var e = newGraphItem();
+		// FIXME: check if defined
+		e.src = nodes_ref[je.src];
+		e.dst = nodes_ref[je.dst];
+		n.attributes = new Object(je.attributes);
+		g.edges[i] = e;
+	}
 }
