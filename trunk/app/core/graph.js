3e13cc77e094bd9ac1ddcacd4f3efb99cda500bb
silhusk <silhusk@e0888478-d5d9-11de-b7d7-53e004a27ad6>
2009-12-01 11:10:28 +0000

    nodes and edges now have a constructor and some functions

--- trunk/app/core/graph.js
+++ trunk/app/core/graph.js
@@ -52,8 +52,67 @@ THE SOFTWARE.
 	}
 
 
-/** Add functionality to JSDot
+/** Edges API
  * 
+ */
+	
+	/** Edge constructor
+	 * DO NOT USE this, use jsdot.newEdge(src, dst)
+	 */
+	JSDot.prototype.Edge = function(src, dst) {
+		this.src = src;
+		this.dst = dst;
+		this.attributes = {};
+		return this;
+	};
+	
+	/** Returns the node where the edge starts.
+	 * @return Node
+	 */
+	JSDot.prototype.Edge.prototype.getSrc = function(){
+		return this.src;
+	};
+	
+	/** Change edge source.
+	 * @param {Node} src source node
+	 */
+	JSDot.prototype.Edge.prototype.setSrc = function(src){
+		this.src = src;
+	};
+	
+	/** Returns the node where the edge ends.
+	 * @return Node
+	 */
+	JSDot.prototype.Edge.prototype.getDst = function(){
+		return this.dst;
+	};
+	
+	/** Change destination of the edge.
+	 * 
+	 * @param {Node} dst destination node
+	 */
+	JSDot.prototype.Edge.prototype.setDst = function(dst) {
+		this.dst = dst;
+	};
+	
+	JSDot.prototype.Edge.prototype.getAttribute = function(key) {
+		return this.attributes[key];
+	}
+	
+	JSDot.prototype.Edge.prototype.setAttribute = function(key, value) {
+		this.attributes[key] = value;
+	};
+
+
+
+/** Added functionality to JSDot
+ * 
+ */
+
+/** Creates a new node in the graph
+ * 
+ * @param {String} name the name of the node
+ * @return {Node} the new node
  */
 JSDot.prototype.newNode = function(name) {
 	//TODO: check if name already exists
@@ -61,3 +120,15 @@ JSDot.prototype.newNode = function(name) {
 	this.graph.nodes.push(n);
 	return n;
 };
+
+/** Creates an edge connecting node src to dst in the graph.
+ * 
+ * @param {Object} src Source Node
+ * @param {Object} dst Destination Node
+ * @return {Edge} the new edge
+ */
+JSDot.prototype.newEdge = function(src, dst) {
+	var e = new this.Edge(src, dst);
+	this.graph.edges.push(e);
+	return e;
+}
