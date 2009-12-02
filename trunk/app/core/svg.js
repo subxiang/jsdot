d018cd959a7f30b0041715b6d65316db40b216b8
nicos.giuliani <nicos.giuliani@e0888478-d5d9-11de-b7d7-53e004a27ad6>
2009-12-02 17:41:02 +0000

    Merged javascript class SVG in svg.js (back to class definition)

--- trunk/app/core/svg.js
+++ trunk/app/core/svg.js
@@ -22,295 +22,331 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
-
 */	
-	
-	
-	
-	
-//	function Class(){ return function(arguments){ this.init(arguments); } }
-	
-	
-	var JSVG = function(id) {
-	
-		this.root = null;
-		this.designArea = null;
-		this.coords = null;
-		this.selectedObj = null;
-		
-		var svgns = "http://www.w3.org/2000/svg";
-		var xlinkns = "http://www.w3.org/1999/xlink";
-		var xmlns = "http://www.w3.org/XML/1998/namespace";
-		
-		this.xmlns = "http://www.w3.org/2000/svg";
-		this.xlink = "http://www.w3.org/1999/xlink";
-
-	
-		function $e(i){ return document.createElementNS(xmlns, i); };
-		function setAttrs (obj, values){ for (i in values) { obj.setAttributeNS(null, i, values[i]); } };
-		function randColor(){ return "rgb(" + parseInt(Math.random() * 255) + "," + parseInt(Math.random() * 255) + "," + parseInt(Math.random() * 255) + ")"; };
 
 
-		
-		/** Builds the design area and adds event listeners. */
-		this.buildDesignArea = function(){
+var svgns = "http://www.w3.org/2000/svg";
+var xlinkns = "http://www.w3.org/1999/xlink";	
+var xmlns = "http://www.w3.org/2000/svg";
+var xlink = "http://www.w3.org/1999/xlink";
 
-			var self = this;
+function $e(i){ return document.createElementNS(xmlns, i); }
+function $(i) { return document.getElementById(i); };
+function Class(){ return function(arguments){ this.init(arguments); } }
+function setAttrs(obj, values){ for (i in values) { obj.setAttributeNS(null, i, values[i]); } }
+function randColor(){ return "rgb(" + parseInt(Math.random() * 255) + "," + parseInt(Math.random() * 255) + "," + parseInt(Math.random() * 255) + ")"; }
+
+var JSVG = new Class();
+
+JSVG.prototype = {
+
+	root: null,
+	designArea:null,
+	coords:null,
+	selected: null,
+
+	/** Constructor */
+	init: function(id){
+
+		this.container = $(id);
+		this.svgdoc = this.container.ownerDocument;
+		this.svgroot = this.svgdoc.createElementNS(svgns, "svg");
+		
+		this.svgroot.setAttribute("width", window.innerWidth - 5);
+		this.svgroot.setAttribute("height", window.innerHeight - 5);
+		this.svgroot.setAttribute("id", "svgroot");
+		this.svgroot.setAttribute("xmlns", svgns);
+		this.svgroot.setAttribute("xmlns:xlink", xlinkns);
+		this.container.appendChild(this.svgroot);
+		
+		this.root = this.svgroot;
+		JSVG.root = this.root;
+		
+		this.firstClick = false;
+		this.coords = this.root.createSVGPoint();
+     	this.grabPoint = this.root.createSVGPoint();
+
+		this.buildMenu();
+		this.buildDesignArea();
+		
+	},
 	
-			this.designArea = this.Element('rect', { "height": "100%", "width": "85%", "x": "15%", "y": 0, "fill": "#eee", "id": "designArea" });
-			
-			// Drag and drop listeners
-			this.designArea.addEventListener('mousemove', function(evt){ self.drag(evt); },false);
-			this.designArea.addEventListener('mouseup', function(evt){ self.drop(evt); },false);
-			// Add elements listener
-			this.designArea.addEventListener('click',function(evt){ self.drawElement(evt); },false);
-						
-		};
+	/** Builds the design area and adds event listeners. */
+	buildDesignArea: function(){
+
+		var self = this;
+
+		this.designArea = new this.Element('rect', { "height": "100%", "width": "85%", "x": "15%", "y": 0, "fill": "#eee", "id": "designArea" });
 		
-		/**
-		 * Draw an element depending on the selected form
-		 * @param {Object} evt
-		 */
-		this.drawElement = function(evt){
-		
-			var self = this;
-			
-			switch(this.selectedObj) {
-				
-				// Creates a rectangle in the design area
-				case 'rect':
-				
-					this.Element('rect', {
-						"height": "5em",
-						"width": "5em",
-						"x": evt.clientX,
-						"y": evt.clientY,
-						"fill": "#ff0000",
-						"id": this.toString()
-						
-					}).addEventListener('mousedown', function(evt){ self.grab(evt); }, false);
+		// Drag and drop listeners
+		this.designArea.addEventListener('mousemove', function(evt){ self.drag(evt); },false);
+		this.designArea.addEventListener('mouseup', function(evt){ self.drop(evt); },false);
+
+		// Add elements listener
+		this.designArea.addEventListener('click',function(evt){ self.drawElement(evt); },false);
 					
-				break;
-				
-				// Creates a circle in the design area
-				case 'circle':
-				
-					self.Element('circle', {
-							"r": "2.5em",
-							"cx": evt.clientX,
-							"cy": evt.clientY,
-							"fill": "#ddd",
-							"fill-opacity": 0.85,
-							"stroke": "#000",
-							"stroke-opacity": 0.85
-					}).addEventListener('mousedown', function(evt){ self.grab(evt); }, false);
-									
-				break;
-			}
-		};
-
-		/**
-		 * Set the element to be dragged.
-		 * @param {Object} evt
-		 */
-		this.grab = function(evt){
-
-			var targetElement = evt.target;
-			
-			if (this.designArea != targetElement) {
-
-				this.selectedObj = targetElement;
-				setAttrs(targetElement, {"fill-opacity": 0.5});
-								
-				// Calculates the element's coords
-	            var transMatrix = targetElement.getCTM();
-				this.getCoords(evt);
-	            this.grabPoint.x = this.coords.x - Number(transMatrix.e);
-	            this.grabPoint.y = this.coords.y - Number(transMatrix.f);
-				
-				// Set out target
-				this.dragElement = targetElement;
-				//this.dragElement.parentNode.appendChild(this.dragElement);
-				this.dragElement.setAttributeNS(null, 'pointer-events', 'none');
-
-			}
-		};
-
-		/**
-		 * Drag the element throught the design area
-		 * @param {Object} evt
-		 */
-		this.drag = function(evt){
-
-	         if (this.dragElement) {
-			 	this.getCoords(evt);
-	            var newX = this.coords.x - this.grabPoint.x;
-           		var newY = this.coords.y - this.grabPoint.y;
-	            this.dragElement.setAttributeNS(null, 'transform', 'translate(' + newX + ',' + newY + ')');
-	         }
-		};
-
-		/**
-		 * Drop the element after mouseup event
-		 * @param {Object} evt
-		 */
-		this.drop = function(evt){
-
-			if (this.dragElement != null) {
-
-				// Set the selected style
-				setAttrs(this.selectedObj, {"fill-opacity": 1});
-				
-				this.dragElement.setAttributeNS(null, 'pointer-events', 'all');
-				this.dragElement = null, this.selectedObj = null;
-			} 
-		};
+	},
+	
+	/**
+	 * Draw an element depending on the selected form
+	 * @param {Object} evt
+	 */
+	drawElement: function(evt){
+	
+		var self = this, newElement;
+		if(!this.selected || !this.selected.name) return;
 		
-		/**
-		 * Get and set the true coordinates
-		 * @param {Object} evt
-		 */
-		this.getCoords = function(evt){
+		switch(this.selected.name) {
 			
-			var scale = this.root.currentScale, translation = this.root.currentTranslate;
+			// Creates a rectangle in the design area
+			case 'rect':
 			
-			this.coords.x = (evt.clientX - translation.x) / scale;
-			this.coords.y = (evt.clientY - translation.y) / scale;
-		};
-
-		/** Builds the left menu, buttons and all listeners */
-		this.buildMenu = function(){
-
-			var self = this; this.toggle = false, this.cnt;
+				newElement = new this.Element('rect', {
+					"height": "5em",
+					"width": "5em",
+					"x": evt.clientX,
+					"y": evt.clientY,
+					"fill": "#ff0000"
+				});
+				
+			break;
 			
-			this.cnt = this.Element('g', { 'id': 'leftMenu', 'width':'20%', 'height':'100%' });			
-			this.bg = this.Element('rect',{ "height": "100%", "width": "100%", "x": 0, "y": 0, "fill": "#333"},this.cnt);
+			// Creates a circle in the design area
+			case 'circle':
+			
+				newElement = new this.Element('circle', {
+					"r": "2.5em",
+					"cx": evt.clientX,
+					"cy": evt.clientY,
+					"fill": "#ddd",
+					"stroke": "#000"
+				
+				})
+				/**
+				 * Needs to find a solution to have different event and function associated
+				.addEventListener('click', function(evt){ 
+				
+					if (self.selected && self.selected.name == 'arrow') {
+						self.drawElement(evt);
+					}
+				}, false);
+				**/
+			
+			break;
 
-			// <-- Rectangle button
-			this.Element('rect',{
+
+			// Creates an arrow between two nodes
+			case 'arrow':
 				
-				"height": "5em",
-				"width": "5em",
-				"x": ".5em",
-				"y": ".5em",
-				'stroke': 'red',
-				"fill": "#ddd",
-				"id": "rectBtn"
-				
-			}, this.cnt).addEventListener('click',function(evt){
-				
-				if (self.selectedObj) {
-					setAttrs(self.selectedObj, {
-						'stroke': 'red'
-					});
+				if (!this.firstClick) {
+					this.x1 = evt.clientX;
+					this.y1 = evt.clientY;
+					this.firstClick = true;
+					return;
 				}
-				// Select the object
-				self.selectedObj = 'rect';
-				setAttrs(this, {"stroke": "yellow"});
 				
-			},false);		
-			// -->
-
-			// <-- Circle button
-			this.Element('circle', {
-				"r": "2.5em",
-				"cx": "9.5em",
-				"cy": "3em",
-				"fill": "#ddd",
-				"fill-opacity": 0.85,
-				"stroke": "red",
-				"stroke-opacity": 0.85,
-				"id": this.toString()
+				new this.Element('line', {
+					"x1": this.x1,
+					"y1": this.y1,
+					"x2": evt.clientX,
+					"y2": evt.clientY,
+					"style": "fill:none;stroke:black;stroke-width:1;"
+				});
 				
-			}, this.cnt).addEventListener('click',function(evt){
-				
-				if (self.selectedObj) {
-					setAttrs(self.selectedObj, { 'stroke': 'red' });
-				}
-				// Select the object
-				self.selectedObj = 'circle';
-				setAttrs(this, { "stroke": "yellow" });
-				
-			},false);	
-			// -->
-
-			this.Element('rect',{
-				
-				"height": "100%",
-				"width": ".3%",
-				"x": "14.7%",
-				"y": 0,
-				"fill": "#222",
-				"id": "toggle"
-				
-			},this.cnt).addEventListener('click',function(){ 
-			
-				setAttrs(self.cnt,{'style':'-15%'});
-	
-			},false);
-		};
-	
-		/**
-		 * Creates and append an element
-		 * @param {String} element
-		 * @param {Object} attrs
-		 * @param {Object} target (Optional)
-		 */
-		this.Element = function(element, attrs){
-
-		  //  var el = this.$e(element);
-		    var el = document.createElementNS(this.xmlns, element);
-		    setAttrs(el, attrs);
-		    arguments[2] ? arguments[2].appendChild(el) : this.root.appendChild(el);
-			
-			return el;
+				this.firstClick = false;
+								
+			break;
 		}
 		
-		
-		
-		/** Constructor
-		 * Creates the svg environment inside the div passed by id.
-		 * 
-		 */
+		if(newElement) newElement.addEventListener('mousedown', function(evt){ self.grab(evt); }, false);
+	},
 
-			this.container = document.getElementById(id);
-			this.svgdoc = this.container.ownerDocument;
-			this.svgroot = this.svgdoc.createElementNS(svgns, "svg");
-			this.svgroot.setAttribute("width", 640);
-			this.svgroot.setAttribute("height", 480);
-			this.svgroot.setAttribute("id", "svgroot");
-			this.svgroot.setAttribute("xmlns", svgns);
-			this.svgroot.setAttribute("xmlns:xlink", xlinkns);
-			this.container.appendChild(this.svgroot);
-			
-			this.root = this.svgroot;
-			/*
-			this.root = document.createElement('svg');
-			this.root.setAttribute('xmlns', "http://www.w3.org/2000/svg");
-			document.getElementById(id).appendChild(this.root);
-			*/
+	/**
+	 * Set the element to be dragged.
+	 * @param {Object} evt
+	 */
+	grab: function(evt){
+
+		var targetElement = evt.target;
 		
-			//JSVG.root = this.root;.
+		if (this.designArea != targetElement) {
+
+			this.selected = targetElement;
+			setAttrs(targetElement, {"fill-opacity": 0.5});
+							
+			// Calculates the element's coords
+            var transMatrix = targetElement.getCTM();
+			this.getCoords(evt);
+            this.grabPoint.x = this.coords.x - Number(transMatrix.e);
+            this.grabPoint.y = this.coords.y - Number(transMatrix.f);
+			
+			// Set out target
+			this.dragElement = targetElement;
+			this.dragElement.setAttributeNS(null, 'pointer-events', 'none');
+
+		}
+	},
+
+	/**
+	 * Drag the element throught the design area
+	 * @param {Object} evt
+	 */
+	drag: function(evt){
+
+         if (this.dragElement) {
+		 	this.getCoords(evt);
+            var newX = this.coords.x - this.grabPoint.x;
+       		var newY = this.coords.y - this.grabPoint.y;
+            this.dragElement.setAttributeNS(null, 'transform', 'translate(' + newX + ',' + newY + ')');
+         }
+	},
+
+	/**
+	 * Drop the element after mouseup event
+	 * @param {Object} evt
+	 */
+	drop: function(evt){
+
+		if (this.dragElement != null) {
+
+			// Set the selected style
+			setAttrs(this.selected, {"fill-opacity": 1});
+			
+			this.dragElement.setAttributeNS(null, 'pointer-events', 'all');
+			this.dragElement = null; this.selected = null;
+		} 
+	},
 	
-		//	this.coords = this.root.createSVGPoint();
-        // 	this.grabPoint = this.root.createSVGPoint();
+	/**
+	 * Get and set the true coordinates
+	 * @param {Object} evt
+	 */
+	getCoords: function(evt){
+		
+		var scale = this.root.currentScale, translation = this.root.currentTranslate;
+		
+		this.coords.x = (evt.clientX - translation.x) / scale;
+		this.coords.y = (evt.clientY - translation.y) / scale;
+	},
 
-			this.buildMenu();
-			this.buildDesignArea();
+	/** Builds the left menu, buttons and all listeners */
+	buildMenu: function(){
+
+		var self = this; this.toggle = false; this.cnt;
+		
+		this.cnt = new this.Element('g', { 'id': 'leftMenu', 'width':'20%', 'height':'100%' });			
+		this.bg = new this.Element('rect',{ "height": "100%", "width": "100%", "x": 0, "y": 0, "fill": "#333"},this.cnt);
+
+		// <-- Rectangle button
+		new this.Element('rect',{
 			
-			/*
-			var new_layer = this.svgdoc.createElementNS(svgns, "g");
-			var layer_title = this.svgdoc.createElementNS(svgns, "title");
-			layer_title.textContent = name;
-			new_layer.appendChild(layer_title);
-			new_layer = this.svgroot.appendChild(new_layer);
+			"height": "5em",
+			"width": "5em",
+			"x": ".5em",
+			"y": ".5em",
+			'stroke': 'red',
+			"fill": "#ddd",
+			"style":"cursor:pointer"
 			
-			var circle = this.svgdoc.createElementNS(svgns, "circle");
-			circle.setAttribute("r", "26");
-			circle.setAttribute("cy", "74");
-			circle.setAttribute("cx", "40");
-			new_layer.appendChild(circle);
-			*/
+		}, this.cnt).addEventListener('click',function(evt){
 			
-	return this;
+			if (self.selected) {
+				setAttrs(self.selected.obj, {
+					'stroke': 'red'
+				});
+			}
+			// Select the object
+			self.selected = {
+				name: 'rect',
+				obj: this
+			};
+			setAttrs(this, {"stroke": "yellow"});
+			
+		},false);		
+		// -->
+
+		// <-- Circle button
+		new this.Element('circle', {
+			"r": "2.5em",
+			"cx": "9.5em",
+			"cy": "3em",
+			"fill": "#ddd",
+			"fill-opacity": 0.85,
+			"stroke": "red",
+			"stroke-opacity": 0.85,
+			"style":"cursor:pointer"
+			
+		}, this.cnt).addEventListener('click',function(evt){
+			
+			if (self.selected) {
+				setAttrs(self.selected.obj, { 'stroke': 'red' });
+			}
+			// Select the object
+			self.selected = {
+				name: 'circle',
+				obj: this
+			};
+			setAttrs(this, { "stroke": "yellow" });
+			
+		},false);	
+		// -->
+
+
+		// <-- Arrow button
+		new this.Element('line', {
+		
+			"x1": ".5em",
+			"y1": "8em",
+			"x2": "5em",
+			"y2": "12em",
+			"style": "fill:none;stroke:red;fill:#ddd;stroke-width:.2em;cursor:pointer;"
+
+		}, this.cnt).addEventListener('click',function(evt){
+			
+			if (self.selected) {
+				setAttrs(self.selected.obj, { 'stroke': 'red' });
+			}
+			// Select the object
+			self.selected = {
+				name: 'arrow',
+				obj: this
+			};
+			setAttrs(this, { "stroke": "yellow" });
+			
+		},false);	
+		// -->
+
+		// --> Toggle menu bar
+		new this.Element('rect',{
+			
+			"height": "100%",
+			"width": ".3%",
+			"x": "14.7%",
+			"y": 0,
+			"fill": "#222",
+			"id": "toggle",
+			"style":"cursor:pointer"
+			
+		},this.cnt).addEventListener('click',function(){ 
+			
+			setAttrs(self.cnt,{'x':'-15%'});
+
+		},false);
+		// <--
+	},
+
+	/**
+	 * Creates and append an element
+	 * @param {String} element
+	 * @param {Object} attrs
+	 * @param {Object} target (Optional)
+	 */
+	Element: function(element, attrs){
+
+	    var el = $e(element);
+	    setAttrs(el, attrs);
+	    arguments[2] ? arguments[2].appendChild(el) : JSVG.root.appendChild(el);
+		
+		return el;
 	}
+}
