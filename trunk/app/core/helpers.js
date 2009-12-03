63e96a589405526fc3eacf365cd29cfde524113d
nicos.giuliani <nicos.giuliani@e0888478-d5d9-11de-b7d7-53e004a27ad6>
2009-12-03 18:24:56 +0000

    selected object re-implemented using object abstraction (from 100 lines of code to 5!)

---
+++ trunk/app/core/helpers.js
@@ -0,0 +1,37 @@
+/*
+This file is part of the JSDot library 
+ 
+http://code.google.com/p/jsdot/
+
+Copyright (c) 2009 Lucia Blondel, Nicos Giuliani, Carlo Vanini
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/	
+
+
+var svgns = "http://www.w3.org/2000/svg";
+var xlinkns = "http://www.w3.org/1999/xlink";	
+var xmlns = "http://www.w3.org/2000/svg";
+var xlink = "http://www.w3.org/1999/xlink";
+
+function $e(i){ return document.createElementNS(xmlns, i); }
+function $(i) { return document.getElementById(i); };
+function Class(){ return function(arguments){ this.init(arguments); } }
+function setAttrs(obj, values) { for (i in values) { obj.setAttribute(i, values[i]); } }
+function randColor(){ return "rgb(" + parseInt(Math.random() * 255) + "," + parseInt(Math.random() * 255) + "," + parseInt(Math.random() * 255) + ")"; }
