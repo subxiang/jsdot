1b692bb614e047e1a6498c2d7b2256fe137b9a77
nicos.giuliani <nicos.giuliani@e0888478-d5d9-11de-b7d7-53e004a27ad6>
2009-12-10 17:40:41 +0000

    Element extends features: getPos() => return the current element position Note: this features will be used further to implement the "true" position of the target element

--- trunk/app/core/helpers.js
+++ trunk/app/core/helpers.js
@@ -30,20 +30,25 @@ var xlinkns = "http://www.w3.org/1999/xlink";
 var xmlns = "http://www.w3.org/2000/svg";
 var xlink = "http://www.w3.org/1999/xlink";
 
-function $e(i){ 
-	if(arguments[1]) return document.createElement(i);
-	return document.createElementNS(svgns, i); 
+Number.prototype.NaN0 = function(){return isNaN(this)?0:this;}
+// Extends Element --> 
+Element.prototype.appends = function(array) { for(i in array){ this.appendChild(array[i]); }};
+Element.prototype.setAttrs = function(attrs) { for (i in attrs) { this.setAttribute(i, attrs[i]); }};
+Element.prototype.size = function(){ var e = this.style; return { width: parseInt(e.borderLeftWidth || 0) + parseInt(e.borderRightWidth || 0) + this.offsetWidth,height: parseInt(e.borderTopWidth || 0) + parseInt(e.borderBottomWidth || 0) + this.offsetHeight }};
+Element.prototype.getPos = function(){
+    var left = 0,top = 0, e = this.offsetParent;
+    while (e.offsetParent) {
+        left += this.offsetLeft + (this.currentStyle ? (parseInt(this.currentStyle.borderLeftWidth)).NaN0() : 0);
+        top += this.offsetTop + (this.currentStyle ? (parseInt(this.currentStyle.borderTopWidth)).NaN0() : 0);
+		e = this.offsetParent
+    }
+    left += this.offsetLeft + (this.currentStyle ? (parseInt(this.currentStyle.borderLeftWidth)).NaN0() : 0);
+    top += this.offsetTop + (this.currentStyle ? (parseInt(this.currentStyle.borderTopWidth)).NaN0() : 0);
+    return { x: left, y: top }
 }
-Element.prototype.appends = function(array) {
-	for(i in array){
-		this.appendChild(array[i]);
-	}
-}
-Element.prototype.setAttrs = function(attrs) {
-	for (i in attrs) {
-		this.setAttribute(i, attrs[i]);
-	}
-}
+// <--
+
+function $e(i){ if(arguments[1]) return document.createElement(i); return document.createElementNS(svgns, i);};
 function $(i) { return document.getElementById(i); };
 function Class(){ return function(arguments){ this.init(arguments); } }
 function setAttrs(obj, values) { for (i in values) { obj.setAttribute(i, values[i]); } }
